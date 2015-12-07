fc452b7bbc1cbdc92cc234a980adee12551b5042
adrianua <Adrian@MacBook-Air.local>
2015-12-08 00:17:15 +0200

    Setup files
    setup initial files

---
+++ get-and-clean/run_analysis.R
@@ -0,0 +1,13 @@
+# if (!file.exists('data')) {
+#   dir.create('data')
+# }
+# 
+# fileURL <- 
+#   'https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip'
+# 
+# download.file(fileURL, destfile = '.data/dataset.zip', method = 'curl')
+# list.files('./data')
+# 
+# dateDownloaded <- date()
+# dateDownloaded
+# 
